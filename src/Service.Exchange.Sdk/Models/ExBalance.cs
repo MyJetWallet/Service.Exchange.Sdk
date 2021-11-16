b8fc5ced620dc6526675b04635439b434a060e81
Dmitrii Krasovskikh <dmitry.krasovskih@gmail.com>
2021-11-16 18:00:26 +0200

    - add version;

--- src/Service.Exchange.Sdk/Models/ExBalance.cs
+++ src/Service.Exchange.Sdk/Models/ExBalance.cs
@@ -6,15 +6,18 @@ namespace Service.Exchange.Sdk.Models
     [DataContract]
     public class ExBalance
     {
+        public static readonly string KeySeparator = "::";
+        
         [DataMember(Order = 1)] public string WalletId { get; set; }
         [DataMember(Order = 2)] public string AssetId { get; set; }
         [DataMember(Order = 3)] public decimal Balance { get; set; }
         [DataMember(Order = 4)] public decimal ReserveBalance { get; set; }
         [DataMember(Order = 5)] public DateTime LastUpdate { get; set; }
+        [DataMember(Order = 6)] public long Version { get; set; }
 
         public string WalletIdAssetId
         {
-            get => $"{WalletId}::{AssetId}";
+            get => $"{WalletId}{KeySeparator}{AssetId}";
             // ReSharper disable once ValueParameterNotUsed
             set { }
         }
